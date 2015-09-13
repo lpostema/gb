403c80c4e64774f86c46ce94c11a48a60e504743
ldp <luke.postema@gmail.com>
2015-09-12 20:00:14 -0400

    Added in starting and ending page finding

--- mpg.py
+++ mpg.py
@@ -5,6 +5,8 @@ import openpyxl
 tree = ET.parse('Meeting Plan Generator.xml')
 root = tree.getroot()
 
+from PyPDF2.PyPDF2.pdf import PdfFileReader
+
 while 1:
 	today = datetime.date.today()
 	daysToNextWednesday = datetime.timedelta((2 - datetime.date.weekday(today)) % 7)
@@ -19,14 +21,52 @@ while 1:
 		continue
 	break
 
-
-
 nextWednesdayMonth = str(nextWednesday.month) if (nextWednesday.month >= 10) else '0' + str(nextWednesday.month)
 nextWednesdayDay = str(nextWednesday.day) if (nextWednesday.day >=10) else '0' + str(nextWednesday.day)
 wednesdaysDate = str(nextWednesday.year) + '-' + nextWednesdayMonth + '-' + nextWednesdayDay
 
+lessonFile = open('..\\2015-16 Gopher Buddies Bible Study.pdf', 'rb')
+lessonPDF = PdfFileReader(lessonFile)
+"""        TOCpage = lessonPDF.getPage(3)
+        TOCstring = TOCpage.extractText()
+        print(TOCstring)"""
 
 
+
+shutil.copyfile('Meeting Planner.xlsx', wednesdaysDate + '\Meeting Planner.xlsx')
+
+yearwb         = openpyxl.load_workbook('..\\2015 Lesson Plan.xlsx', data_only=True )
+yearwb_formula = openpyxl.load_workbook('..\\2015 Lesson Plan.xlsx', data_only=False)
+calendarws = yearwb.get_sheet_by_name("Calendar")
+calendarws_formula = yearwb_formula.get_sheet_by_name("Calendar")
+versesws = yearwb.get_sheet_by_name("Verses")
+meetingDates = tuple(calendarws.iter_rows('A2:A39'))
+for meetingDate in meetingDates:
+    if meetingDate[0].value.date() == nextWednesday:
+        break
+meetingDate = meetingDate[0]
+nextWednesdayRow = meetingDate.row
+nextWednesdayLesson = calendarws[('C' + str(nextWednesdayRow))].value
+
+#find lesson in pdf
+
+for i in range(15, 363):
+        pdfpage = lessonPDF.getPage(i)
+        pdfstring = pdfpage.extractText()
+        if nextWednesdayLesson in pdfstring:
+                lessonStartPage = i+1
+                break
+
+#find range of pages
+pdfstring = pdfstring[:pdfstring.rfind('\n')]  #string page number off
+pdfLessonString = pdfstring[pdfstring.rfind('\n'):]
+
+for i in range(lessonStartPage+1, 363):
+        pdfpage = lessonPDF.getPage(i)
+        pdfstring = pdfpage.extractText()
+        if pdfLessonString not in pdfstring:
+                lessonEndPage = i
+                break
 
 for plugin in root.findall('plugin'):
     for filename in plugin.findall('filelist'):
@@ -38,6 +78,11 @@ for plugin in root.findall('plugin'):
                     newString += wednesdaysDate
                     newString += '\\Meeting Planner.pdf'
                     file.set('name', newString)
+            if 'pageselection' in file.attrib.keys():
+                startIndex = file.attrib['pageselection'].find('change-page')
+                if startIndex != -1:
+                    newString = str(lessonStartPage) + '-' + str(lessonEndPage)
+                    file.set('pageselection', newString)
     for destination in plugin.findall('destination'):
         #print(destination.tag, destination.attrib)
         if 'value' in destination.attrib.keys():
@@ -53,21 +98,8 @@ try:
 except FileExistsError:
     print("Directory already exists.")
 tree.write(wednesdaysDate + '\Meeting Plan Generator 2.xml')
-shutil.copyfile('Meeting Planner.xlsx', wednesdaysDate + '\Meeting Planner.xlsx')
 
-yearwb         = openpyxl.load_workbook('..\\2014 Lesson Plan.xlsx', data_only=True )
-yearwb_formula = openpyxl.load_workbook('..\\2014 Lesson Plan.xlsx', data_only=False)
-calendarws = yearwb.get_sheet_by_name("Calendar")
-calendarws_formula = yearwb_formula.get_sheet_by_name("Calendar")
-versesws = yearwb.get_sheet_by_name("Verses")
-meetingDates = tuple(calendarws.iter_rows('A2:A39'))
-for meetingDate in meetingDates:
-    if meetingDate[0].value == nextWednesday:
-        break
-meetingDate = meetingDate[0]
-nextWednesdayRow = meetingDate.row
-nextWednesdayLesson = calendarws[('C' + str(nextWednesdayRow))].value
-
+        
 plannerwb = openpyxl.load_workbook(wednesdaysDate + '\\Meeting Planner.xlsx')
 meetingws = plannerwb.get_sheet_by_name('Meeting Overview')
 meetingws['E10'] = nextWednesdayLesson
@@ -86,6 +118,7 @@ meetingws['C7'] = versesws['E' + verseRow].value
 meetingws['D8'] = versesws['D' + verseRow].value
 # make number format different for date
 meetingws['G1'].number_format = '[$-409]mmmm\\ d\\,\\ yyyy;@'
+lessonFile.close()
 try:
 	plannerwb.save(wednesdaysDate + '\\Meeting Planner2.xlsx')
 except:
